35d1d8d6aec6d53623a48ebd060e7286ebf4e465
HoneyBadger <marat@genatulin.com>
2020-12-19 17:21:22 +0300

    Task 3 completed.

---
+++ lesson_4_3.py
@@ -0,0 +1,10 @@
+"""
+3. Для чисел в пределах от 20 до 240 найти числа, кратные 20 или 21. Необходимо решить задание в одну строку.
+"""
+
+start_number = 20
+finish_number = 240
+multiple = 20
+
+output_list = [item for item in range(start_number, finish_number + 1) if item % multiple == 0]
+print(output_list)
